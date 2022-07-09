8f1f7995b1e81f7e5d65907e62c0eabfbf7f2755
horiyu <horiyu.dev@gmail.com>
2022-07-09 23:02:16 +0900

    [add]confirmation of DC motor operation

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,38 @@
 #include <Arduino.h>
-#include "PRIZM.h"
+#include <PRIZM.h>
+#include "chassis.hpp"
+
+PRIZM prizm;
+EXPANSION exc;
 
 void setup() {
+  
+  prizm.PrizmBegin();
+  exc.setServoSpeeds(2,25,25,25,25,25,25);
 
 }
 
 void loop() {
+
+  exc.setMotorSpeed(1,1,500);
+  exc.setMotorSpeed(1,2,500);
+  exc.setMotorSpeed(3,1,-500);
+  exc.setMotorSpeed(3,2,-500);
+  delay(1000);
+  exc.setMotorSpeed(1,1,500);
+  exc.setMotorSpeed(1,2,-500);
+  exc.setMotorSpeed(3,1,500);
+  exc.setMotorSpeed(3,2,-500);
+  delay(1000);
+  exc.setMotorSpeed(1,1,-500);
+  exc.setMotorSpeed(1,2,-500);
+  exc.setMotorSpeed(3,1,500);
+  exc.setMotorSpeed(3,2,500);
+  delay(1000);
+  exc.setMotorSpeed(1,1,-500);
+  exc.setMotorSpeed(1,2,500);
+  exc.setMotorSpeed(3,1,-500);
+  exc.setMotorSpeed(3,2,500);
+  delay(1000);
 
 }
